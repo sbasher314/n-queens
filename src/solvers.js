2cf3580e456beacf63bc0670db00d95dfa3e2920
Jason Carini <carinij@gmail.com>
2021-01-23 13:06:27 -0800

    Partial parallel/web workers implementation

--- src/solvers.js
+++ src/solvers.js
@@ -98,35 +98,56 @@ window.findNQueensSolution = function(n) {
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
 
-  if (n === 0) {
+  if (n === 0 || n === 1) {
     return 1;
   }
 
   let solutionCount = 0;
   let testBoard = new Board({'n': n});
 
-  var recursiveSolutionFinder = function (board, rowIndex) {
-    let isRow0 = (rowIndex === 0);
-    let adjustedN = isRow0 ? n / 2 : n;
-    for (let colIndex = 0; colIndex < adjustedN; colIndex++) {
-      board.togglePiece(rowIndex, colIndex);
-      if (!board.hasColConflictAt(colIndex)
-      && !board.hasMajorDiagonalConflictAt(colIndex - rowIndex)
-      && !board.hasMinorDiagonalConflictAt(colIndex + rowIndex)) {
-        if (rowIndex === n - 1) {
-          solutionCount++;
-        } else {
-          recursiveSolutionFinder(board, rowIndex + 1);
+  if (window.Worker) {
+    let workersCalled = 0;
+    const queenWorker = new Worker('src/recursiveSolutionFinder.js');
+    console.log(queenWorker);
+    queenWorker.postMessage(['spawn', testBoard, 0]);
+    queenWorker.onmessage = function (message) {
+      console.log('queenWorker received a message!');
+      console.log(message.data);
+      let solutionCountFromWorkers;
+      if (message.data[0] === 'result') {
+        solutionCountFromWorkers = message.data[1];
+      }
+      workersCalled--;
+      solutionCount += solutionCountFromWorkers;
+      if (workersCalled === 0) {
+        queenWorker.terminate;
+      }
+    };
+
+  } else {
+    let recursiveSolutionFinder = function (board, rowIndex) {
+      let isRow0 = (rowIndex === 0);
+      let adjustedN = isRow0 ? n / 2 : n;
+      for (let colIndex = 0; colIndex < adjustedN; colIndex++) {
+        board.togglePiece(rowIndex, colIndex);
+        if (!board.hasColConflictAt(colIndex)
+        && !board.hasMajorDiagonalConflictAt(colIndex - rowIndex)
+        && !board.hasMinorDiagonalConflictAt(colIndex + rowIndex)) {
+          if (rowIndex === n - 1) {
+            solutionCount++;
+          } else {
+            recursiveSolutionFinder(board, rowIndex + 1);
+          }
+        }
+        board.togglePiece(rowIndex, colIndex);
+        if (isRow0 && colIndex === Math.floor(n / 2) - 1) {
+          solutionCount = solutionCount * 2;
         }
       }
-      board.togglePiece(rowIndex, colIndex);
-      if (isRow0 && colIndex === Math.floor(n / 2) - 1) {
-        solutionCount = solutionCount * 2;
-      }
-    }
-  };
+    };
+    recursiveSolutionFinder(testBoard, 0);
+  }
 
-  recursiveSolutionFinder(testBoard, 0);
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
 };
